dd6ad3a16b4c21b299d007462d9dd131d4128905
Komadir0011 <newkoder7@gmail.com>
2023-05-10 18:15:15 +0500

    botter commit

---
+++ main.js
@@ -0,0 +1,17 @@
+const TelegramBot = require('node-telegram-bot-api');
+
+require('dotenv').config();
+
+
+const TOKEN =process.env.TOKEN;
+
+const bot = new TelegramBot(TOKEN, {polling: true});
+
+
+
+bot.on('message', (msg) => {
+  const chatId = msg.chat.id;
+
+
+  bot.sendMessage(chatId, 'Received your message');
+});
